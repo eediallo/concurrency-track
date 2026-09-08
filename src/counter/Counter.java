a3ccbe3185f13c61162e5e0f9c318cc78247fd4e
eedialllo <elhhabdouldiallo@gmail.com>
2026-09-08 12:51:21 +0100

    simulate Thread interference using a a counter

---
+++ src/counter/Counter.java
@@ -0,0 +1,47 @@
+package counter;
+
+public class Counter {
+    private int counter = 0;
+
+    public void increment() {
+        counter++;
+    }
+
+    public void decrement() {
+        counter--;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    static void main() throws InterruptedException {
+        Counter counter = new Counter();
+        int iterations = 100_000;
+
+        // Thread 1
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < iterations; i++) {
+                counter.increment();
+            }
+        });
+
+        // Thread 2
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < iterations; i++) {
+                counter.decrement();
+            }
+        });
+
+        // start both threads concurrently
+        t1.start();
+        t2.start();
+
+        // wait for both threads to finish before reading the result
+        t1.join();
+        t2.join();
+
+        int c = counter.getCounter();
+        System.out.println("Final Counter Value: " + c);
+    }
+}
